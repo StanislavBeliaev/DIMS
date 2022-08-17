1c6ca9ff70d578996bbcfd62a54106cd62d524e3
Stanislav Beliaev <testerbeliaev@gmail.com>
2022-08-17 16:16:12 +0300

    Firebase Update

--- src/index.jsx
+++ src/index.jsx
@@ -4,34 +4,51 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-e867zKVObWp4xno5RIw4TNIk2qgGon4",
-  authDomain: "dims-e0abc.firebaseapp.com",
-  projectId: "dims-e0abc",
-  storageBucket: "dims-e0abc.appspot.com",
-  messagingSenderId: "1052199020912",
-  appId: "1:1052199020912:web:73019d06d9e643654a07b5",
-  measurementId: "G-GXY779PFRW"
+  apiKey: 'AIzaSyB-e867zKVObWp4xno5RIw4TNIk2qgGon4',
+
+  authDomain: 'dims-e0abc.firebaseapp.com',
+
+  databaseURL: 'https://dims-e0abc-default-rtdb.europe-west1.firebasedatabase.app',
+
+  projectId: 'dims-e0abc',
+
+  storageBucket: 'dims-e0abc.appspot.com',
+
+  messagingSenderId: '1052199020912',
+
+  appId: '1:1052199020912:web:73019d06d9e643654a07b5',
+
+  measurementId: 'G-GXY779PFRW',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+// Initialize Firebase Authentication and get a reference to the service
+const db = getFirestore(app);
+const auth = getAuth(app);
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-<BrowserRouter>
+  <BrowserRouter>
     <App />
-</BrowserRouter>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
