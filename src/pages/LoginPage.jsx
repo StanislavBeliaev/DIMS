25bea3529b1f169a3b0bd05b7e021e08384c01e7
Stanislav Beliaev <testerbeliaev@gmail.com>
2022-08-24 16:31:48 +0300

    add login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fire from '../fire';
+import classes from './pages.module.css';
 
 function LoginPage() {
-    <div>LoginPage</div>;
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('Отправлена форма');
+    }
+
+    return (
+        <div className={classes.LoginPage__Container}>
+            <h1 className={classes.SignIn}>Sign In</h1>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label>Username: </label>
+                    <input type='email' name='email' placeholder='email' required />
+                    <label>Password: </label>
+                    <input type='password' name='password' placeholder='password' required />
+                    <input type='submit' />
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default LoginPage;
